d44439efe681b5dc2b1410a2383b50cd851f3c4e
“n15g” <nick@nicks-mail.com>
2019-10-10 08:22:55 +1100

    Fixes #7 - [Hero Corps Recruit] is missing villain name [Hero Corps Reject]

--- src/badge/exploration/hero-corps-recruit.ts
+++ src/badge/exploration/hero-corps-recruit.ts
@@ -1,10 +1,13 @@
-import {ALIGNMENT_HERO, BadgeType, IBadgeData} from "coh-content-db";
+import {ALIGNMENT_HERO, Alternate, BadgeType, IBadgeData} from "coh-content-db";
 import {SteelCanyon} from "../../map/steel-canyon";
 
 export const HeroCorpsRecruit: IBadgeData = {
     type: BadgeType.EXPLORATION,
     key: "hero-corps-recruit",
-    names: [{value: "Hero Corps Recruit"}],
+    names: [
+        {type: Alternate.H, value: "Hero Corps Recruit"},
+        {type: Alternate.V, value: "Hero Corps Reject"},
+        ],
     alignment: ALIGNMENT_HERO,
     mapKey: SteelCanyon.key,
     location: [-1764.0, 16.0, 1364.0],
@@ -14,7 +17,8 @@ export const HeroCorpsRecruit: IBadgeData = {
     notes: "The Hero Corps Recruit Badge is in the Steel Canyon Medical Center neighborhood." +
         "\n\nIt is located just outside and to the right of the door to the hospital, at the southeastern corner of the building.",
     links: [
-        {title: "Hero Corps Recruit Badge", href: "https://paragonwiki.com/wiki/Hero_Corps_Recruit_Badge"}
+        {title: "Hero Corps Recruit Badge", href: "https://paragonwiki.com/wiki/Hero_Corps_Recruit_Badge"},
+        {title: "Hero Corps Reject Badge", href: "https://paragonwiki.com/wiki/Hero_Corps_Reject_Badge"}
     ],
     icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/hero.png"}],
     vidiotMapKey: "2"
--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -7,6 +7,8 @@ export class HomecomingChangelog implements IServerGroupData {
 
         "0.3.0": "* https://github.com/n15g/coh-content-db-homecoming/issues/1 - AE badges were all incorrectly labeled as achievements.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/6 - [Trusting] and [Arriviste] are now separate badges.\n" +
-            "* https://github.com/n15g/coh-content-db-homecoming/issues/3 - Badge icons are now urls instead of keys."
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/3 - Badge icons are now urls instead of keys.",
+
+        "0.3.1": "* https://github.com/n15g/coh-content-db-homecoming/issues/7 - [Hero Corps Recruit] is missing villain name [Hero Corps Reject]\n"
     };
 }
