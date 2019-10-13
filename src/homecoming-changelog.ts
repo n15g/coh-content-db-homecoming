5b3f656365b0b5f89995cfa234d1848049f4a897
“n15g” <nick@nicks-mail.com>
2019-10-14 09:06:12 +1100

    0.3.1 changelog

--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -9,6 +9,7 @@ export class HomecomingChangelog implements IServerGroupData {
             "* https://github.com/n15g/coh-content-db-homecoming/issues/6 - [Trusting] and [Arriviste] are now separate badges.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/3 - Badge icons are now urls instead of keys.",
 
-        "0.3.1": "* https://github.com/n15g/coh-content-db-homecoming/issues/7 - [Hero Corps Recruit] is missing villain name [Hero Corps Reject]\n"
+        "0.3.1": "* https://github.com/n15g/coh-content-db-homecoming/issues/8 - Finished the exploration badge list.\n" +
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/7 - [Hero Corps Recruit] is missing villain name [Hero Corps Reject].\n"
     };
 }
