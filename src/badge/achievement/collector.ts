c74dffd868b04df4efebe7704eb0603794b23048
“n15g” <nick@nicks-mail.com>
2019-10-17 08:42:33 +1100

    Fixes #14 - [Collector] badge has incorrect badge text and link

--- src/badge/achievement/collector.ts
+++ src/badge/achievement/collector.ts
@@ -10,13 +10,13 @@ export const Collector: IBadgeData = {
     ],
     alignment: ALIGNMENT_ANY,
     badgeText: [
-        {type: Alternate.H, value: "You have earned 10 badges. You are officially a tourist in Paragon City."},
+        {type: Alternate.H, value: "You have earned 25 badges."},
         {type: Alternate.V, value: "You've earned enough badges to be called a Native."},
         {type: Alternate.P, value: "25 badges later, it's clear now nothing will keep you from uncovering the secrets of Praetoria and beyond."}
     ],
     acquisition: "Earn this badge by earning 25 badges.",
     links: [
-        {title: "Tourist Badge", href: "https://paragonwiki.com/wiki/Collector_Badge"},
+        {title: "Collector Badge", href: "https://paragonwiki.com/wiki/Collector_Badge"},
         {title: "Native Badge", href: "https://paragonwiki.com/wiki/Native_Badge"},
         {title: "Extractor of Secrets Badge", href: "https://paragonwiki.com/wiki/Extractor_of_Secrets_Badge"},
     ],
--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -3,19 +3,27 @@ import {IServerGroupData} from "coh-content-db";
 export class HomecomingChangelog implements IServerGroupData {
     public readonly key: string = "homecoming";
     public readonly changelog: { [id: string]: string } = {
-        "0.2.0": "* Test release.",
+        "0.2.0":
+            "* Test release.",
 
-        "0.3.0": "* https://github.com/n15g/coh-content-db-homecoming/issues/1 - AE badges were all incorrectly labeled as achievements.\n" +
+        "0.3.0":
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/1 - AE badges were all incorrectly labeled as achievements.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/6 - [Trusting] and [Arriviste] are now separate badges.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/3 - Badge icons are now urls instead of keys.",
 
-        "0.3.1": "* https://github.com/n15g/coh-content-db-homecoming/issues/8 - Finished the exploration badge list.\n" +
+        "0.3.1":
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/8 - Finished the exploration badge list.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/7 - [Hero Corps Recruit] is missing villain name [Hero Corps Reject].\n",
 
-        "0.3.2": "* https://github.com/n15g/coh-content-db-homecoming/issues/10 - [Seeker of the Unknown] is listed as [Dark Mystic].\n" +
+        "0.3.2":
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/10 - [Seeker of the Unknown] is listed as [Dark Mystic].\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/11 - [Seeker] is incorrectly listed as [Pathfinder].\n",
 
-        "0.3.3": "* https://github.com/n15g/coh-content-db-homecoming/issues/13 - Gladiator badges.\n" +
+        "0.3.3":
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/13 - Gladiator badges.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/12 - [Guardian Angel/Barely Contained] has wrong name type.\n",
+
+        "0.4.0":
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/14 - [Collector] badge has incorrect badge text and link.\n"
     };
 }
