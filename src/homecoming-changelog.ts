4f3bffab0469e8359a5a59400ce7f7a4823e67c8
“n15g” <nick@nicks-mail.com>
2019-10-24 23:51:13 +1100

    Typo in the changelog.

--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -45,9 +45,9 @@ export class HomecomingChangelog implements IServerGroupData {
         "1.0.0":
             "* https://github.com/n15g/coh-content-db-homecoming/issues/4 - Correct in-game badge ordering.\n",
 
-        "1.0.1":
+        "1.0.2":
             "* https://github.com/n15g/coh-content-db-homecoming/pull/29 - A couple of accolades were out of order.\n" +
-            "* https://github.com/n15g/coh-content-db-homecoming/issues/30 - Begone FoP - The not-actually-a-real-badge [Flames of Prometheus] has been removed." +
+            "* https://github.com/n15g/coh-content-db-homecoming/issues/30 - Begone FoP - The not-actually-a-real-badge [Flames of Prometheus] has been removed.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/31 - Tanker is missing from AT list."
     };
 }
