361784b55e539e0b6be8ae3ff4f58996bf4c116c
Barry Hylton <blhylton@blhylton.com>
2019-11-02 10:30:55 -0400

    Update Pursuer acquisition text.

--- src/badge/event/pursuer.ts
+++ src/badge/event/pursuer.ts
@@ -12,7 +12,7 @@ export const Pursuer: IBadgeData = {
         {type: Alternate.H, value: "Villains have escaped from the Big Zig, and you have taken it upon yourself to track them down."},
         {type: Alternate.V, value: "Ever since you got out of the Big Zig, heroes and do-gooders have hunted you down."},
     ],
-    acquisition: "Not currently obtainable.",
+    acquisition: "Awarded for logging in to the game during November.",
     links: [
         {title: "Pursuer Badge", href: "https://paragonwiki.com/wiki/Pursuer_Badge"},
         {title: "Elusive Badge", href: "https://paragonwiki.com/wiki/Elusive_Badge"}
