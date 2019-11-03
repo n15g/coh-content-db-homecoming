75b2443dfd7391246edab6b784f66bc0efe699bd
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2019-11-03 17:43:14 -0500

    Update craftsman.ts
    Corrected typo in acquisition text, was '5000', changed to '500' per forum post and verified on wiki.

--- src/badge/invention/craftsman.ts
+++ src/badge/invention/craftsman.ts
@@ -7,7 +7,7 @@ export const Craftsman: IBadgeData = {
     names: [{value: "Craftsman"}],
     alignment: ALIGNMENT_ANY,
     badgeText: [{value: "You are a true Craftsman, and have created 500 inventions."}],
-    acquisition: "Craft 5000 items in the Invention System or Supergroup Base system.",
+    acquisition: "Craft 500 items in the Invention System or Supergroup Base system.",
     notes: "This includes anything craftable on Invention Worktables, Base Worktables, and Empowerment Stations (including Empowerment Buffs), as well as special crafting such as the" +
         " Vanguard Crafting Table and Candy Keeper.",
     links: [
