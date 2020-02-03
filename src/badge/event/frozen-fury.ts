b7e289a646cdbd0dfd6220be476fbf597e59a12d
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-02-03 17:12:49 -0500

    Update frozen-fury.ts (#76)
    Added settitle ID

--- src/badge/event/frozen-fury.ts
+++ src/badge/event/frozen-fury.ts
@@ -3,6 +3,7 @@ import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 export const FrozenFury: IBadgeData = {
     type: BadgeType.EVENT,
     key: "frozen-fury",
+    setTitleId: 210,
     names: [
         {value: "Frozen Fury"},
     ],
