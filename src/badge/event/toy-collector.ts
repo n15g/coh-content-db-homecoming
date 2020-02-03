2d9f331e66325743c176ebf5c34ced106529ac3b
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-02-03 15:54:21 -0500

    Update toy-collector.ts
    Added settitle ID

--- src/badge/event/toy-collector.ts
+++ src/badge/event/toy-collector.ts
@@ -3,6 +3,7 @@ import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 export const ToyCollector: IBadgeData = {
     type: BadgeType.EVENT,
     key: "toy-collector",
+    setTitleId: 522,
     names: [
         {value: "Toy Collector"},
     ],
