653480799790d87892d6456491a09c0ff2ffeb1d
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-02-03 17:12:32 -0500

    Update frosty.ts (#77)
    Added settitle ID

--- src/badge/event/frosty.ts
+++ src/badge/event/frosty.ts
@@ -3,6 +3,7 @@ import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 export const Frosty: IBadgeData = {
     type: BadgeType.EVENT,
     key: "frosty",
+    setTitleId: 703,
     names: [
         {value: "Frosty"},
     ],
