55570ca7fc134e325cec5dffa8a11a169b3e1155
Nicholas Armstrong <nick@nicks-mail.com>
2020-03-03 11:12:26 +1100

    Fixed bidirectional dependency issues in test VidiotMap data.

--- src/map/abandoned-sewer-network.ts
+++ src/map/abandoned-sewer-network.ts
@@ -1,14 +1,4 @@
 import {IGameMapData} from "coh-content-db";
-import {SewerNetwork} from "./sewer-network";
-import {BoomtownRefugee} from "../badge/exploration/boomtown-refugee";
-import {HadesAspirant} from "../badge/exploration/hades-aspirant";
-import {SecretOfTheCity} from "../badge/exploration/secret-of-the-city";
-import {SprawlSurvivor} from "../badge/exploration/sprawl-survivor";
-import {TheUnderlord} from "../badge/exploration/the-underlord";
-import {FromBeneathYou} from "../badge/exploration/from-beneath-you";
-import {LivingDark} from "../badge/exploration/living-dark";
-import {PersephoneSupplicant} from "../badge/exploration/persephone-supplicant";
-import {Academic} from "../badge/history/academic";
 
 export const AbandonedSewerNetwork: IGameMapData = {
     key: "abandoned-sewer-network",
@@ -19,24 +9,24 @@ export const AbandonedSewerNetwork: IGameMapData = {
             name: "Abandoned Sewer Network Vidiot Map",
             href: "https://n15g.github.io/coh-content-db-homecoming/images/vidiotmaps/abandoned-sewer-network.png",
             pointsOfInterest: [
-                {x: 295, y: 365, mapKey: SewerNetwork.key},
-                {x: 1113, y: 718, mapKey: SewerNetwork.key},
-                {x: 68, y: 1589, mapKey: SewerNetwork.key},
-                {x: 673, y: 1802, mapKey: SewerNetwork.key},
-                {x: 1430, y: 1407, mapKey: SewerNetwork.key},
+                {x: 295, y: 365, mapKey: "sewer-network"},
+                {x: 1113, y: 718, mapKey: "sewer-network"},
+                {x: 68, y: 1589, mapKey: "sewer-network"},
+                {x: 673, y: 1802, mapKey: "sewer-network"},
+                {x: 1430, y: 1407, mapKey: "sewer-network"},
 
                 {x: 736, y: 1096, notes: "Trial Door"},
 
-                {x: 295, y: 450, badgeKey: BoomtownRefugee.key},
-                {x: 687, y: 1097, badgeKey: HadesAspirant.key},
-                {x: 1054, y: 717, badgeKey: SecretOfTheCity.key},
-                {x: 673, y: 1776, badgeKey: SprawlSurvivor.key},
-                {x: 98, y: 1589, badgeKey: TheUnderlord.key},
-                {x: 1407, y: 1407, badgeKey: FromBeneathYou.key},
-                {x: 563, y: 760, badgeKey: LivingDark.key},
-                {x: 618, y: 1433, badgeKey: PersephoneSupplicant.key},
+                {x: 295, y: 450, badgeKey: "boomtown-refugee"},
+                {x: 687, y: 1097, badgeKey: "hades-aspirant"},
+                {x: 1054, y: 717, badgeKey: "secret-of-the-city"},
+                {x: 673, y: 1776, badgeKey: "sprawl-survivor"},
+                {x: 98, y: 1589, badgeKey: "the-underlord"},
+                {x: 1407, y: 1407, badgeKey: "from-beneath-you"},
+                {x: 563, y: 760, badgeKey: "living-dark"},
+                {x: 618, y: 1433, badgeKey: "persephone-supplicant"},
 
-                {x: 583, y: 744, badgeKey: Academic.key, badgePartialKey: Academic.partials[2].key},
+                {x: 583, y: 744, badgeKey: "academic", badgePartialKey: "acad-2"},
             ]
         }
     ]
