fb8b03ee5007b5e0024daa3bb8c1497189b983df
Nicholas Armstrong <nick@nicks-mail.com>
2020-03-27 12:24:54 +1100

    Wrong quotes used for description.

--- src/badge/exploration/base-jumper.ts
+++ src/badge/exploration/base-jumper.ts
@@ -10,10 +10,10 @@ export const BaseJumper: IBadgeData = {
     mapKey: RiktiWarZone.key,
     location: [3434.0, 0.0, 511.0],
     badgeText: [{value: "The Council have taken over this building as their base of operations. You might want to check your back."}],
-    notes: "The Base Jumper Badge is located in Pork Chop Hill neighborhood of the [map:${RiktiWarZone.key}].\n" +
-        "\n" +
-        "The badge is located in the foyer of a Council-owned building just southwest of the Pork Chop Hill neighborhood marker.\n" +
-        "The building is marked with two large Council banners, with a cream colored foyer, and the marker is located inside behind the reception desk.",
+    notes: `The Base Jumper Badge is located in Pork Chop Hill neighborhood of the [map:${RiktiWarZone.key}].
+    
+The badge is located in the foyer of a Council-owned building just southwest of the Pork Chop Hill neighborhood marker.
+The building is marked with two large Council banners, with a cream colored foyer, and the marker is located inside behind the reception desk.`,
     links: [
         {title: "Base Jumper Badge", href: "https://paragonwiki.com/wiki/Base_Jumper_Badge"},
         {title: "Updated RWZ Vidiot Map", href: "https://forums.homecomingservers.com/topic/969-rikti-war-zone-badges-missing/?do=findComment&comment=27161"}
