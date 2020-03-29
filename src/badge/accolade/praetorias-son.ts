6e478bc1c9fcd0a2b8413a5ee3dd61ed22c01e18
Matt-Fitzpatrick <mattfitz@yahoo.com>
2020-03-28 22:50:13 -0700

    Split Praetoria's Son from V.I.P.
    Splits the Praetoria's Son/Praetoria's Daughter badge from the V.I.P./Destined One badge. Corrects the in-game list order of this badge for Praetorian characters.

---
+++ src/badge/accolade/praetorias-son.ts
@@ -0,0 +1,23 @@
+import {ALIGNMENT_PRAETORIAN, Alternate, BadgeType, IBadgeData} from "coh-content-db";
+
+export const VIP: IBadgeData = {
+    type: BadgeType.ACCOLADE,
+    key: "praetorias-son",
+    setTitleId: 1658,
+    names: [
+        {type: Alternate.MP, value: "Praetoria's Son"},
+        {type: Alternate.FP, value: "Praetoria's Daughter"}
+    ],
+    alignment: ALIGNMENT_PRAETORIAN,
+    badgeText: [
+        {value: `Praetoria is your world, for better or worse.`}
+    ],
+    notes: `Awarded on any character creation`,
+    links: [
+        {title: "Praetoria's Son Badge", href: "https://paragonwiki.com/wiki/Praetoria%27s_Son_Badge"},
+        {title: "Praetoria's Daughter Badge", href: "https://paragonwiki.com/wiki/Praetoria%27s_Daughter_Badge"}
+    ],
+    icons: [
+        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accolade/vip.png"}
+    ]
+};
