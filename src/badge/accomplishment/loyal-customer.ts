57ee3cd89f3e15111e835d0ee502981bd9788a4a
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-05-30 12:23:11 -0400

    Update loyal-customer.ts
    updating badge text

--- src/badge/accomplishment/loyal-customer.ts
+++ src/badge/accomplishment/loyal-customer.ts
@@ -9,8 +9,7 @@ export const LoyalCustomer: IBadgeData = {
     ],
     alignment: ALIGNMENT_ANY,
     badgeText: [
-        {value: "Your diligence hasn't gone without notice and is appreciated.  This badge has been granted to " +
-        "you for earning twenty nine Reward Tokens."}
+        {value: "You have stopped the Sky Raiders from ransacking Wentworth's Fine Consignments."}
     ],
     acquisition: "Complete the Market Crash Task Force in Kallisti Warf.",
     links: [
