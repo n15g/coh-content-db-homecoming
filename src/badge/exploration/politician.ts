c81020ba4f3a9d0b62538f9d7f467e558fcb64c2
Matt-Fitzpatrick <mattfitz@yahoo.com>
2020-11-25 17:31:30 -0800

    Siren's Call map rotation (i27)
    Updated coordinates and compass directions in hint text after Siren's Call map rotation. (i27)

--- src/badge/exploration/politician.ts
+++ src/badge/exploration/politician.ts
@@ -8,9 +8,9 @@ export const Politician: IBadgeData = {
     names: [{value: "Politician"}],
     alignment: ALIGNMENT_ANY,
     mapKey: SirensCall.key,
-    location: [961.0, 0.0, -665.0],
+    location: [664.0, 0.0, 960.5],
     badgeText: [{value: "Council man John Chard announced from this location that his island would be the front lines of defense against the Rikti invasion."}],
-    notes: "The Politician Badge is located in the [map:${SirensCall.key}] zone to the right (east) of the doorway of the building immediately south of the hero base.",
+    notes: "The Politician Badge is located in the [map:${SirensCall.key}] zone to the right (north) of the doorway of the building immediately east of the hero base.",
     links: [{title: "Politician Badge", href: "https://paragonwiki.com/wiki/Politician_Badge"}],
     icons: [
         {type: Alternate.H, value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/hero.png"},
