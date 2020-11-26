fc482cff4813cab479e411c4ae7dfc5b57882f34
Nicholas Armstrong <nick@nicks-mail.com>
2020-11-27 08:13:44 +1100

    Typo in changelog.

--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -136,8 +136,8 @@ export class HomecomingChangelog implements IServerGroupData {
             "* https://github.com/n15g/coh-content-db-homecoming/pull/118 - Updated coordinates for [Lorekeeper], and acquisition text for [Avid Reader].\n",
         "1.3.0":
             "* https://github.com/n15g/coh-content-db-homecoming/pull/120 - i27 updates to old badges.\n" +
-            "* https://github.com/n15g/coh-content-db-homecoming/pull/121 - Siren's Call rotation (i27)\n" +
-            "* https://github.com/n15g/coh-content-db-homecoming/pull/122 - New i27 badge basics\n.",
+            "* https://github.com/n15g/coh-content-db-homecoming/pull/121 - Siren's Call rotation (i27).\n" +
+            "* https://github.com/n15g/coh-content-db-homecoming/pull/122 - New i27 badge basics.\n",
         "1.3.1":
             "* https://github.com/n15g/coh-content-db-homecoming/issues/123 - [Ensorcelled] badge has wrong Vidiot id.\n"
     };
