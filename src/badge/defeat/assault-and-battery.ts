1cff74f78e646a1d1d5e2fdfbe0477e719e06db1
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 09:21:25 -0500

    Update assault-and-battery.ts
    adding missing data

--- src/badge/defeat/assault-and-battery.ts
+++ src/badge/defeat/assault-and-battery.ts
@@ -3,9 +3,19 @@ import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 export const AssaultAndBattery: IBadgeData = {
     type: BadgeType.DEFEAT,
     key: "assault-and-battery",
+    setTitleId: 2446,
     names: [
         {value: "Assault and Battery"}
     ],
     alignment: ALIGNMENT_ANY,
-    acquisition: "Defeat 50 Freakshow Super Stunners."
+    badgeText: [
+        {value: `Despite their propensity for getting back up, you have taught these Freakshow a thing or two about knowing when to stay down.`}
+    ],
+    acquisition: "Defeat 50 Freakshow Super Stunners.",
+    links: [
+        {title: "Assault and Battery Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Assault_and_Battery_Badge"}
+    ],
+    icons: [
+        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/assault-and-battery.png"}
+    ],
 };
