fa990e76b4e1e34675d969979852d9191f9e36d3
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 12:01:34 -0500

    Update containment-specialist.ts
    fixing tabs

--- src/badge/accomplishment/containment-specialist.ts
+++ src/badge/accomplishment/containment-specialist.ts
@@ -9,13 +9,13 @@ export const ContainmentSpecialist: IBadgeData = {
     ],
     alignment: ALIGNMENT_HERO,
     badgeText: [
-		{value: `Under extreme pressure, you were cool in a crisis. With only five minutes in a warehouse engulfed in flames, you were able to disable the rigged sprinklers before the situation became a national tragedy.`}
-	],
+	{value: `Under extreme pressure, you were cool in a crisis. With only five minutes in a warehouse engulfed in flames, you were able to disable the rigged sprinklers before the situation became a national tragedy.`}
+    ],
     acquisition: "Turn off the sprinklers before the timer runs out during The Graveyard Shift story arc.",
     links: [
-		{title: "Containment Specialist Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Containment_Specialist_Badge"}
-	],
-	icons: [
-		{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/containment-specialist.png"}
-	],
+	{title: "Containment Specialist Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Containment_Specialist_Badge"}
+    ],
+    icons: [
+	{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/containment-specialist.png"}
+    ],
 };
