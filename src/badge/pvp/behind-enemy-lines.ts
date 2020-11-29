9a8fca9218c2a258c9eee44f21a7b1bd6627d7a2
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 12:14:07 -0500

    Update behind-enemy-lines.ts
    fix indentation

--- src/badge/pvp/behind-enemy-lines.ts
+++ src/badge/pvp/behind-enemy-lines.ts
@@ -12,15 +12,15 @@ export const BehindEnemyLines: IBadgeData = {
     ],
     alignment: ALIGNMENT_ANY,
     badgeText: [
-		{type: Alternate.H, value: "You have slipped past Arachnos' defenses and thwarted their plans against Freedom Corps."},
+	{type: Alternate.H, value: "You have slipped past Arachnos' defenses and thwarted their plans against Freedom Corps."},
         {type: Alternate.V, value: "You have slipped past Freedom Corps' defenses and disrupted their plans against Arachnos."}
-	],
+    ],
     acquisition: `Complete at least one door mission in [map:${BloodyBay.key}], [map:${SirensCall.key}] and [map:${Warburg.key}].`,
     notes: "One mission per zone, patrols don't count.",
     links: [
-		{title: "Behind Enemy Lines Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Behind_Enemy_Lines_Badge"}
-	],
-	icons: [
-		{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/pvp/cracked.png"}
-	],
+	{title: "Behind Enemy Lines Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Behind_Enemy_Lines_Badge"}
+    ],
+    icons: [
+	{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/pvp/cracked.png"}
+    ],
 };
