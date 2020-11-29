fb9f33afd0fd70df5ec4bf607d0bac4697ad95f1
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 12:12:01 -0500

    Update green-machine.ts
    fix indentation

--- src/badge/defeat/green-machine.ts
+++ src/badge/defeat/green-machine.ts
@@ -9,13 +9,13 @@ export const GreenMachine: IBadgeData = {
     ],
     alignment: ALIGNMENT_ANY,
     badgeText: [
-		{value: `You have gone above and beyond to obliterate the Warhulks of the Nemesis army, keeping the streets safe from their steam-powered terror. Amongst the Nemesis Army, you are often deemed 'Unpopular When Irritated'.`}
-	],
+	{value: `You have gone above and beyond to obliterate the Warhulks of the Nemesis army, keeping the streets safe from their steam-powered terror. Amongst the Nemesis Army, you are often deemed 'Unpopular When Irritated'.`}
+    ],
     acquisition: "Defeat 100 Nemesis Warhulks.",
     links: [
-		{title: "Green Machine Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Green_Machine_Badge"}
-	],
-	icons: [
-		{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/green-machine.png"}
-	],
+	{title: "Green Machine Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Green_Machine_Badge"}
+    ],
+    icons: [
+	{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/green-machine.png"}
+    ],
 };
