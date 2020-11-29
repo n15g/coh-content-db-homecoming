abae4085c2172b1be0c216aae9d52fe99d836188
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 12:17:00 -0500

    Update behind-enemy-lines.ts
    fix indentation

--- src/badge/pvp/behind-enemy-lines.ts
+++ src/badge/pvp/behind-enemy-lines.ts
@@ -13,7 +13,7 @@ export const BehindEnemyLines: IBadgeData = {
     alignment: ALIGNMENT_ANY,
     badgeText: [
 	{type: Alternate.H, value: "You have slipped past Arachnos' defenses and thwarted their plans against Freedom Corps."},
-        {type: Alternate.V, value: "You have slipped past Freedom Corps' defenses and disrupted their plans against Arachnos."}
+	{type: Alternate.V, value: "You have slipped past Freedom Corps' defenses and disrupted their plans against Arachnos."}
     ],
     acquisition: `Complete at least one door mission in [map:${BloodyBay.key}], [map:${SirensCall.key}] and [map:${Warburg.key}].`,
     notes: "One mission per zone, patrols don't count.",
