4e2219d1072bdcb4f82ae644b85aac92c26987ab
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-11-29 12:04:11 -0500

    Update patient-zero.ts
    fix indentation

--- src/badge/accomplishment/patient-zero.ts
+++ src/badge/accomplishment/patient-zero.ts
@@ -9,13 +9,13 @@ export const PatientZero: IBadgeData = {
     ],
     alignment: ALIGNMENT_VILLAIN,
     badgeText: [
-		{value: `Doc Buzzsaw bit off more she could chew when she called out for help against Cortex, leading you into a whole new world of mad science and pushing fringe Freakshow members and desperate terminal Eidolons to evolve beyond their humanity as conscripts in a new cybertech army. Piecemeal owes their existence to you.`}
-	],
+	{value: `Doc Buzzsaw bit off more she could chew when she called out for help against Cortex, leading you into a whole new world of mad science and pushing fringe Freakshow members and desperate terminal Eidolons to evolve beyond their humanity as conscripts in a new cybertech army. Piecemeal owes their existence to you.`}
+    ],
     acquisition: "Complete Doc Buzzsaw's arc, The Freakish Lab of Dr. Vahzilok.",
     links: [
-		{title: "Patient Zero Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Patient_Zero_Badge"}
-	],
-	icons: [
-		{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/patient-zero.png"}
-	],
+	{title: "Patient Zero Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Patient_Zero_Badge"}
+    ],
+    icons: [
+	{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/patient-zero.png"}
+    ],
 };
