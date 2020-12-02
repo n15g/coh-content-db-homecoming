2ec92daab598372c346d2b1a710d077315a8ad4b
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2020-12-01 23:01:12 -0500

    Update unabashed.ts
    added note that the badge is now available in both the instanced and normal version of the zone, and corrected number of pylons per HC wiki

--- src/badge/exploration/unabashed.ts
+++ src/badge/exploration/unabashed.ts
@@ -10,9 +10,9 @@ export const Unabashed: IBadgeData = {
     mapKey: RiktiWarZone.key,
     location: [2947.0, -18.0, -5638.0],
     badgeText: [{value: "If you've made it onto the Rikti Ship, under the defensive shields, you're either very brave or....."}],
-    notes: "The Unabashed Badge is located in the center of the Rikti ship in the [map:${RiktiWarZone.key}].\n" +
+    notes: "The Unabashed Badge is located in the center of the Rikti ship in both the instanced and normal versions of the [map:${RiktiWarZone.key}].\n" +
         "\n" +
-        "The badge marker only be found when the shields have been taken down by destroying all 18 Rikti Pylons.",
+        "The badge marker only be found when the shields have been taken down by destroying all 20 Rikti Pylons.",
     links: [
         {title: "Unabashed Badge", href: "https://paragonwiki.com/wiki/Unabashed_Badge"},
         {title: "Updated RWZ Vidiot Map", href: "https://forums.homecomingservers.com/topic/969-rikti-war-zone-badges-missing/?do=findComment&comment=27161"}
