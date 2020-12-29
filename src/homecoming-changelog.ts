52cd2182279dec658a7b5bfcfec8eb9664118c96
Nicholas Armstrong <nick@nicks-mail.com>
2020-12-30 09:34:41 +1100

    1.3.3 Changelog

--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -144,5 +144,8 @@ export class HomecomingChangelog implements IServerGroupData {
         "1.3.2":
             "* https://github.com/n15g/coh-content-db-homecoming/pull/126 - i27 updates part 1.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/pull/127 - i27 updates part 2.\n",
+        "1.3.3":
+            "* https://github.com/n15g/coh-content-db-homecoming/pull/128 - Updated [Headjuiced] and [Starstruck] history badges to Any alignment to reflect fact that they can be earned by any alignment.\n" +
+            "* https://github.com/n15g/coh-content-db-homecoming/pull/128 - Updated [Swashbuckler] history badge to correct coordinates of plaque in Siren's Call after that zone was rotated in i27.\n"
     };
 }
