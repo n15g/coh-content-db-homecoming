ffe5384587c6c58334d0a124432856b33c5087f0
Matt-Fitzpatrick <mattfitz@yahoo.com>
2021-04-20 09:45:35 -0700

    New Stone Cold icon (#6)

--- src/badge/accomplishment/stone-cold.ts
+++ src/badge/accomplishment/stone-cold.ts
@@ -16,6 +16,6 @@ export const StoneCold: IBadgeData = {
         {title: "Stone Cold Badge", href: "https://paragonwiki.com/wiki/Stone_Cold_Badge"}
     ],
     icons: [
-        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/stature-2.png"}
+        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/stature-1.png"}
     ],
 };
