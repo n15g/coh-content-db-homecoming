87036ed53c6b8e06f4d99a72f5d6b921cd6be468
Chris Clarke <332873+CheeseTheMonkey@users.noreply.github.com>
2021-04-29 22:06:37 +0100

    Add ts file for the 17th anniversary badge

---
+++ src/badge/event/legendary.ts
@@ -0,0 +1,19 @@
+import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
+
+export const Celebrant: IBadgeData = {
+    type: BadgeType.EVENT,
+    key: "legendary",
+    setTitleId: 2460,
+    names: [
+        {value: "Legendary"},
+    ],
+    alignment: ALIGNMENT_ANY,
+    badgeText: [
+        {value: "You have helped celebrate the 17th anniversary of City of Heroes."},
+    ],
+    acquisition: "Available for purchase from Luna in Ouroboros during the anniversary event in May.",
+    links: [
+        {title: "Legendary Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Legendary_Badge"}
+    ],
+    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/legendary.png"}]
+};
