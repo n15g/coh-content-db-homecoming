ed646ab60b4ec45422cad7929fd77739c85c3e91
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2021-05-01 10:54:13 -0400

    Update resurgent.ts
    updated acquisition notes

--- src/badge/event/resurgent.ts
+++ src/badge/event/resurgent.ts
@@ -10,5 +10,5 @@ export const Resurgent: IBadgeData = {
     alignment: ALIGNMENT_ANY,
     badgeText: [{value: `You have helped celebrate the 16th anniversary of City of Heroes.`}],
     icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/resurgent.png"}],
-    acquisition: "Auto-awarded during May 2020, and thereafter available for purchase from Luna in Ouroboros during the anniversary event in May."
+    acquisition: "Available for purchase from Luna in Ouroboros during the anniversary event in May."
 };
