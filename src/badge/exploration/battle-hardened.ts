5e7e14a4f2a94941fe6d657adf0ad6b113031762
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2021-05-15 15:55:53 -0400

    Update battle-hardened.ts
    Updated badge name per recent change in-game, added acquisition notes and link to HC wiki

--- src/badge/exploration/battle-hardened.ts
+++ src/badge/exploration/battle-hardened.ts
@@ -5,11 +5,15 @@ export const BattleHardened: IBadgeData = {
     type: BadgeType.EXPLORATION,
     key: "battle-hardened",
     setTitleId: 2371,
-    names: [{value: "Battle Hardened"}],
+    names: [{value: "Battle-Hardened"}],
     alignment: ALIGNMENT_ANY,
     badgeText: [{value: `The loyal legions of Cimerora have prevailed in battle against an enemy wielding unimaginable power.`}],
     mapKey: Cimerora.key,
     location: [-1006.2, 265.5, 3113.5],
+    notes: "Located in [map:cimerora] on the north edge of the map on ramparts above two enormous double doors. It is around 400 yards north of Proximo.",
+    links: [
+        {title: "Battle-Hardened Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Battle-Hardened_Badge"}
+    ],
     icons: [
         {type: Alternate.H, value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/hero.png"},
         {type: Alternate.V, value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/villain.png"},
