096e6300e69a62fc7e5ad654e062ca6f56d101dc
Nicholas Armstrong <nick@nicks-mail.com>
2021-06-04 12:40:40 +1000

    #143 changes

--- src/homecoming-changelog.ts
+++ src/homecoming-changelog.ts
@@ -170,5 +170,7 @@ export class HomecomingChangelog implements IServerGroupData {
         "1.3.11":
             "* https://github.com/n15g/coh-content-db-homecoming/pull/142 - Update [Battle-Hardened] badge.\n" +
             "* https://github.com/n15g/coh-content-db-homecoming/issues/113 - Switch links from ParagonWiki to Homecoming Wiki.\n",
+        "1.3.12":
+            "* https://github.com/n15g/coh-content-db-homecoming/pull/143 - Rename [Shopkeeper] tp [Proprietor].\n",
     };
 }
