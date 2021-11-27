a00bdb9d3c1c59a90615b8880628b2cd7561654a
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2021-11-26 23:48:18 -0500

    Update battle-hardened.ts
    updating badge info

--- src/badge/exploration/battle-hardened.ts
+++ src/badge/exploration/battle-hardened.ts
@@ -9,8 +9,8 @@ export const BattleHardened: IBadgeData = {
     alignment: ALIGNMENT_ANY,
     badgeText: [{value: `The loyal legions of Cimerora have prevailed in battle against an enemy wielding unimaginable power.`}],
     mapKey: Cimerora.key,
-    location: [-1006.2, 265.5, 3113.5],
-    notes: "Located in [map:cimerora] on the north edge of the map on ramparts above two enormous double doors. It is around 400 yards north of Proximo.",
+    location: [-1084.0, 172.2, 4690.0],
+    notes: `Located in [map:cimerora] on a rampart overlooking the location where Imperious stands, 140 yards south of the Cimerora neighborhood marker.`,
     links: [
         {title: "Battle-Hardened Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Battle-Hardened_Badge"}
     ],
