c5a915ce3cfb702104d72c16f4058fe5db6b6269
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2021-12-01 08:27:30 -0500

    Create license-to-chill.ts
    Adding winter event 2021 login badge, which shares an icon with the 2020 winter event login badge

---
+++ src/badge/event/license-to-chill.ts
@@ -0,0 +1,19 @@
+import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
+
+export const LicenseToChill: IBadgeData = {
+    type: BadgeType.EVENT,
+    key: "license-to-chill",
+    setTitleId: 2492,
+    names: [
+        {value: "License to Chill"},
+    ],
+    alignment: ALIGNMENT_ANY,
+    badgeText: [
+        {value: `Winter is a time when things slow down. Relax. Take it easy. Chill.`},
+    ],
+    acquisition: "Login during the 2021 Winter event.",
+    links: [
+        {title: "License to Chill Badge", href: "https://hcwiki.cityofheroes.dev/wiki/License_to_Chill_Badge"}
+    ],
+    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/winter_login.png"}]
+};
