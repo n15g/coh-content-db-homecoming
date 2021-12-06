b91001a60ba8db331916927edf29c5c31051013d
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2021-12-06 14:39:10 -0500

    Update joyful.ts
    updating purchase cost

--- src/badge/event/joyful.ts
+++ src/badge/event/joyful.ts
@@ -11,7 +11,7 @@ export const Joyful: IBadgeData = {
     badgeText: [
         {value: "The power of the season finds you overcome with joy."},
     ],
-    acquisition: "Select as a reward for completing Father Time's mission during the Winter Event or purchase from the Candy Keeper during the Winter Event for 20 Candy Canes and 5000 inf.",
+    acquisition: "Select as a reward for completing Father Time's mission during the Winter Event or purchase from the Candy Keeper during the Winter Event for 25 Candy Canes and 30,000 inf.",
     links: [
         {title: "Joyful Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Joyful_Badge"}
     ],
