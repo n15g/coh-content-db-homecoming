f78de4e5fce1bc1b4934b43b754c271c151ff4af
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-07-31 14:44:22 -0400

    Add files via upload
    adding files for new badges

---
+++ src/badge/accolade/temporal-warrior.ts
@@ -0,0 +1,21 @@
+import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
+
+export const TemporalWarrior: IBadgeData = {
+    type: BadgeType.ACCOLADE,
+    key: "temporal-warrior",
+    setTitleId: 1629,
+    names: [
+        {value: "Temporal Warrior"}
+    ],
+    alignment: ALIGNMENT_ANY,
+    badgeText: [
+        {value: `Your connection to your home timeline has been severed. Your only purpose now is eternal battle for a future you will never reach.`},
+    ],
+    notes: `Awarded on creation of a PvP-only Temporal Warrior character.`,
+    links: [
+        {title: "Temporal Warrior Badge", href: "https://homecoming.wiki/wiki/Temporal_Warrior_Badge"},
+    ],
+    icons: [
+        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accolade/char-creation.png"}
+    ]
+};
