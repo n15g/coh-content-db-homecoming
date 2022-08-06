87168ed0cf034bb969cb0b59f166ce2257804768
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-08-06 09:59:07 -0400

    Update doppelganger.ts
    typo fix

--- src/badge/achievement/doppelganger.ts
+++ src/badge/achievement/doppelganger.ts
@@ -11,7 +11,7 @@ export const Doppelganger: IBadgeData = {
     badgeText: [
         {value: `You've purchased five permanent costume powers using the Prismatic Aether you assembled from overcoming many challenges.`}
     ],
-    acquisition: `Use Prismatic Aether salvage to purchase a total of five permanent costume power from BenevoLabs vendors in any Vault Reserve location`,
+    acquisition: `Use Prismatic Aether salvage to purchase a total of five permanent costume powers from BenevoLabs vendors in any Vault Reserve location`,
     links: [
         {title: "Doppelganger Badge", href: "https://homecoming.wiki/wiki/Doppelganger_Badge"}
     ],
