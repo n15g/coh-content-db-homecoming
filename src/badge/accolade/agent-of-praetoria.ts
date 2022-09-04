060ed3826b9f467f97729e8cec23616d80f0a98e
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-09-04 17:24:39 -0400

    Update agent-of-praetoria.ts
    updating acquisition and notes

--- src/badge/accolade/agent-of-praetoria.ts
+++ src/badge/accolade/agent-of-praetoria.ts
@@ -13,7 +13,7 @@ export const AgentOfPraetoria: IBadgeData = {
         {value: `You stand on the fragile tipping point between Order and Chaos. Which way will you go?`}
     ],
     acquisition: `Complete every story arc in [map:${NovaPraetoria.key}]`,
-    notes: `This badge rewards 20 reward merits`,
+    notes: `This badge rewards 20 reward merits.`,
     links: [
         {title: "Agent of Praetoria Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Agent_of_Praetoria_Badge"}
     ],
