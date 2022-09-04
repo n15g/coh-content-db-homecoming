f600407d9e9f536e03ad2b42ebc25c6b26857f87
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-09-04 18:28:06 -0400

    Update vigilant.ts
    updating acquisition and notes

--- src/badge/accolade/vigilant.ts
+++ src/badge/accolade/vigilant.ts
@@ -13,7 +13,15 @@ export const Vigilant: IBadgeData = {
         {type: Alternate.H, value: `You're always there when the people of Paragon City need you.`},
         {type: Alternate.V, value: `Your shadow looms over not only the Rogue Isles and Paragon City, but the entire world.`}
     ],
-    notes: `Complete every story arc in the [map:${RiktiWarZone.key}] to earn this badge. [Rewards 20 Merits]`,
+    acquisition: `Complete every story arc in [map:${RiktiWarZone.key}]`,
+    notes: `
+This badge rewards 20 reward merits for completing the following story arcs (contact in parentheses):
+* Welcome to Vanguard (Levantera)
+* The Strange Case of Benjamin A. Decker (Levantera)
+* Dreams of Peace and Acts of War (Serpent Drummer)
+* The Red and the Black (Gaussian)
+* The Horror of War (The Dark Watcher)
+`,
     links: [
         {title: "Vigilant Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Vigilant_Badge"}
     ],
