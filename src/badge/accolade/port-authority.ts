9cf5512f03d60413de55dcdc628de2f543a3b3d6
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-09-04 17:27:03 -0400

    Update port-authority.ts
    updating acquisition and notes

--- src/badge/accolade/port-authority.ts
+++ src/badge/accolade/port-authority.ts
@@ -13,7 +13,7 @@ export const PortAuthority: IBadgeData = {
         {value: `You've obtained this accolade by completing every story arc within Striga Isle.`}
     ],
     acquisition: `Complete every story arc in [map:${StrigaIsle.key}]`,
-    notes: `This badge rewards 20 reward merits`,
+    notes: `This badge rewards 20 reward merits.`,
     links: [
         {title: "Port Authority Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Port_Authority_Badge"}
     ],
