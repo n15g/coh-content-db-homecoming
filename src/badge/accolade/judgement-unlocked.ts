5407f4818db939fd87920b1c5485cc80ff81a746
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-09-04 10:16:12 -0400

    Update judgement-unlocked.ts
    updating acquisiton and notes

--- src/badge/accolade/judgement-unlocked.ts
+++ src/badge/accolade/judgement-unlocked.ts
@@ -11,7 +11,8 @@ export const JudgementUnlocked: IBadgeData = {
     badgeText: [
         {value: `You unlocked your Judgement Incarnate slot allowing you to create powerful new abilities that can be slotted there.`}
     ],
-    notes: `Unlock your Judgement Incarnate slot`,
+    acquisition: `Unlock the Judgement Incarnate slot`,
+    notes: `Unlock the Judgement Incarnate slot by earning Incarnate Experience through normal play.`,
     links: [
         {title: "Judgement Unlocked Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Judgement_Unlocked_Badge"}
     ],
