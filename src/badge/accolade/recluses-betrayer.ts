6e42b2c4beb7dc4812311dc8ab23ae24c21eab3c
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2022-09-04 17:23:15 -0400

    Update recluses-betrayer.ts
    updating acquisition and notes

--- src/badge/accolade/recluses-betrayer.ts
+++ src/badge/accolade/recluses-betrayer.ts
@@ -13,8 +13,9 @@ export const ReclusesBetrayer: IBadgeData = {
         {type: Alternate.H, value: `Lord Recluse was wrong to place his trust in you.`},
         {type: Alternate.V, value: `You are the most villainous of villains and I, Lord Recluse, am proud of you!`}
     ],
+    acquisition: `Complete every story arc from Black Scorpion, Captain Mako, Ghost Widow, Scirocco, and Lord Recluse`,
+    notes: `This badge rewards 20 reward merits.`,
     icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accolade/recluses-betrayer.png"}],
-    notes: `Complete every story arc from Black Scorpion, Captain Mako, Ghost Widow, Scirocco and Lord Recluse to earn this badge. [Rewards 20 Merits]`,
     links: [
         {title: "Recluse's Betrayer Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Recluse%27s_Betrayer_Badge"},
         {title: "Recluse's Right Hand Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Recluse%27s_Right_Hand_Badge"}
