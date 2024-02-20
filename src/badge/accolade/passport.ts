962a02fad9ddb999d8c9830faa1a73f3e9e45f35
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2024-02-20 15:58:14 -0500

    Update passport.ts
    Updated acquisition requirements

--- src/badge/accolade/passport.ts
+++ src/badge/accolade/passport.ts
@@ -12,10 +12,10 @@ export const Passport: IBadgeData = {
     badgeText: [
         {value: "Your travels have taken you to mysterious and exotic places. You have faced many dangers, but now you have returned to the place you call home."},
     ],
-    acquisition: `Acquire any zone exploration accolade by collecting all 8 exploration badges in a zone with a supergroup base portal, or earn the [badge:${PocketDVipGoldClubMember.key}] badge.`,
+    acquisition: `Acquire any ten exploration badges, or earn the [badge:${PocketDVipGoldClubMember.key}] badge.`,
     notes: "Upon acquiring this badge, a character will earn the Long Range Teleporter power.",
     links: [
-        {title: "Passport Badge", href: "https://hcwiki.cityofheroes.dev/wiki/Passport_Badge"}
+        {title: "Passport Badge", href: "https://homecoming.wiki/wiki/Passport_Badge"}
     ],
     icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/accolade/passport.png"}]
 };
