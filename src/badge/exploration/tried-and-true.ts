fb6aa02b03f5591eab66e09ffed300fb6d63f460
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2024-02-20 13:42:36 -0500

    Update tried-and-true.ts
    Updating info

--- src/badge/exploration/tried-and-true.ts
+++ src/badge/exploration/tried-and-true.ts
@@ -7,16 +7,15 @@ export const TriedAndTrue: IBadgeData = {
     names: [{value: "Tried and True"}],
     alignment: ALIGNMENT_HERO,
     badgeText: [{
-        value: "When the Omega Team accepted their mission to the Rikti home dimension, they knew they likely wouldn't be coming back." + "" +
-            " You have protected their memorial through many trials, staying true to the end."
+        value: `When the Omega Team accepted their mission to the Rikti home dimension, they knew they likely wouldn't be coming back. You have protected their memorial through many trials, staying true to the end.`
     }],
-    notes: `You need to visit several memorial plaques across a few missions and zones to aquire this badge:
+    notes: `Visit the Omega Team memorial plaques in three separate instances:
 
-* Visit the open-world Memorial in Atlas Park City Hall.
-* Visit the Memorial during the Cape Mission (Available at level 20) from the City Representative in Atlas Park City Hall.
-* Visit the Memorial during the Dr. Kahn Task Force.`,
+* The open-world Memorial in Atlas Park City Hall (66.0, -634.2, -681.9).
+* During the Cape Mission (Available at level 20) from the City Representative in Atlas Park City Hall (1520.0, -813.2, -2688.1).
+* During the Dr. Kahn Task Force (1520.0, -813.2, -2688.1).`,
     links: [
-        {title: "New Hero One Memorial Badge", href: "https://forums.homecomingservers.com/topic/1889-new-hero-one-memorial-badge/?do=findComment&comment=38908"}
+        {title: "Tried and True Badge", href: "https://homecoming.wiki/wiki/Tried_and_True_Badge"},
     ],
     icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/hero.png"}]
 };
