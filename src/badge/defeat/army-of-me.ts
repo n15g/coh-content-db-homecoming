6f49c48bf36199c5911e84a4ffb68624b61c9990
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2024-02-22 21:37:27 -0500

    Update army-of-me.ts
    Added Ouro arc ID

--- src/badge/defeat/army-of-me.ts
+++ src/badge/defeat/army-of-me.ts
@@ -13,8 +13,8 @@ export const ArmyOfMe: IBadgeData = {
             value: `You don't understand the math behind it, but you're pretty sure you're equal to or greater than eight of yourself.`
         }
     ],
-    acquisition: `Defeat eight clones of yourself in the 'Good Villains Never Die' (Level 25-29, Arc XX.XX in Ouro, Villain) story arc from Leonard`,
-    notes: `During the first mission (called 'Test Out Your Clones') of the 'Good Villains Never Die' (Level 25-29, Arc XX.XX in Ouro, Villain) story arc from Leonard, speak with Dr. Higgins. From his dialogue tree, select "Send out all eight clones", verify your choice, and then defeat all eight clones.`,
+    acquisition: `Defeat eight clones of yourself in the 'Good Villains Never Die' (Level 25-29, Arc 17.04 in Ouro, Villain) story arc from Leonard`,
+    notes: `During the first mission (called 'Test Out Your Clones') of the 'Good Villains Never Die' (Level 25-29, Arc 17.04 in Ouro, Villain) story arc from Leonard, speak with Dr. Higgins. From his dialogue tree, select "Send out all eight clones", verify your choice, and then defeat all eight clones.`,
     links: [
         {title: "Army of Me Badge", href: "https://homecoming.wiki/wiki/Army_of_Me_Badge"}
     ],
