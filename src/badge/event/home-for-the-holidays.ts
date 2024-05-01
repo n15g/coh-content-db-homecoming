c5de003f000d04787baa68daaf6eab06bf7ead29
AboveTheChemist <57296525+AboveTheChemist@users.noreply.github.com>
2024-05-01 13:32:14 -0400

    Update home-for-the-holidays.ts
    Updated acquisition text

--- src/badge/event/home-for-the-holidays.ts
+++ src/badge/event/home-for-the-holidays.ts
@@ -11,7 +11,7 @@ export const HomeForTheHolidays: IBadgeData = {
     badgeText: [
         {value: `You feel the warm and comforting sense of returning to a familiar place filled with cherished friends and traditions.`},
     ],
-    acquisition: "Login during the 2023 Winter event.",
+    acquisition: "Purchase from the Candy Keeper for 25 Candy Canes and 30,000 inf.",
     links: [
         {title: "Home for the Holidays Badge", href: "https://homecoming.wiki/wiki/Home_for_the_Holidays_Badge"}
     ],
