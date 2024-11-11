d40b6900bc134326a70c2975f6bc4df4b6ef936b
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-11-11 15:41:34 -0700

    Update adept.ts

--- src/badge/achievement/adept.ts
+++ src/badge/achievement/adept.ts
@@ -12,11 +12,11 @@ export const Adept: IBadgeData = {
         {
             value: "The amount of moral options open to you sometimes leads to compulsory or contradictory decisions, but that's part of the freedom to make choices. You've completed several Alignment Tip missions and made some moral decisions."}
     ],
-    acquisition: "Earn this badge by completing 10 Alignment Tip missions.",
+    acquisition: "Complete 10 Alignment Tip missions.",
     links: [
         {title: "Adept Badge", href: "https://homecoming.wiki/wiki/Adept_Badge"}
     ],
     icons: [
-        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/achievement/alignment-mission.png"}
+        {value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/achievement/alignment-tips.png"}
     ],
 };
