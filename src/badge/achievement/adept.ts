8c80ea450ed9a2bdc8ba02bb0cebf40d50a762d0
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-11-11 15:46:02 -0700

    Update adept.ts

--- src/badge/achievement/adept.ts
+++ src/badge/achievement/adept.ts
@@ -10,7 +10,7 @@ export const Adept: IBadgeData = {
     alignment: ALIGNMENT_ANY,
     badgeText: [
         {
-            value: "The amount of moral options open to you sometimes leads to compulsory or contradictory decisions, but that's part of the freedom to make choices. You've completed several Alignment Tip missions and made some moral decisions."}
+            value: `The amount of moral options open to you sometimes leads to compulsory or contradictory decisions, but that's part of the freedom to make choices. You've completed several Alignment Tip missions and made some moral decisions.`}
     ],
     acquisition: "Complete 10 Alignment Tip missions.",
     links: [
