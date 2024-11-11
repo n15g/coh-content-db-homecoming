7ba218169468b57cd2194eccb2176b76b9b20ee8
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-11-11 15:45:59 -0700

    Update multifaceted.ts

--- src/badge/achievement/multifaceted.ts
+++ src/badge/achievement/multifaceted.ts
@@ -10,7 +10,7 @@ export const Multifaceted: IBadgeData = {
     alignment: ALIGNMENT_ANY,
     badgeText: [
         {
-            value: "You've walked too many roads of life to count, and your experience affords you a position of reflection and wisdom. You've completed countless Alignment Tip missions and made many moral decisions."}
+            value: `You've walked too many roads of life to count, and your experience affords you a position of reflection and wisdom. You've completed countless Alignment Tip missions and made many moral decisions.`}
     ],
     acquisition: "Complete 25 Alignment Tip missions.",
     links: [
