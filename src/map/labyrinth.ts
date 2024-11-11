cc051b1a77909e78d17cd4f6b8869b55e7ba4dfd
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-11-11 14:12:51 -0700

    Update and rename labyrinth.ts to the-labyrinth-of-fog.ts

--- src/map/labyrinth.ts
+++
@@ -1,8 +0,0 @@
-import {IGameMapData} from "coh-content-db";
-
-export const LabyrinthMap: IGameMapData = {
-    key: "labyrinth-map",
-    name: "Labyrinth Map",
-    links: [{title: "Labyrinth of Fog", href: "https://homecoming.wiki/wiki/Lab_Rat_Badge"}]
-    links: [{title: "Labyrinth of Fog", href: "https://homecoming.wiki/wiki/Hidden_In_Fog_Badge"}]
-};
---
+++ src/map/the-labyrinth-of-fog.ts
@@ -0,0 +1,7 @@
+import {IGameMapData} from "coh-content-db";
+
+export const TheLabyrinthOfFog: IGameMapData = {
+    key: "the-labyrinth-of-fog",
+    name: "The Labyrinth Of Fog",
+    links: [{title: "The Labyrinth Of Fog", href: "https://homecoming.wiki/wiki/The_Labyrinth_of_Fog"}]
+};
