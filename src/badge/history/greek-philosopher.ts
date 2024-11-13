7efd8395a9593f040e13ba269aa5097eede27762
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-11-13 08:42:34 -0700

    Update greek-philosopher.ts

--- src/badge/history/greek-philosopher.ts
+++ src/badge/history/greek-philosopher.ts
@@ -31,16 +31,16 @@ export const GreekPhilosopher: IBadgeData = {
         {
             key: "gree-1",
             type: BadgePartialType.PLAQUE,
-            mapKey: Labyrinth.key,
+            mapKey: TheLabyrinthOfFog.key,
             plaqueType: PlaqueType.MONUMENT,
             location: [227.5, -84.0, 2669.2],
             inscription: `This divine prison would inherit the strength of those within. An inescapable maze constructed by human imagination. Entrusted to the rulers of the land, the calamity was hidden. The labyrinth shackling the catastrophic fog was forbidden.`,
-            notes: `This plaque is Core of The Labyrinth. It is located in front of the Gate of Darkness pillar.`,
+            notes: `This plaque is Core of The Labyrinth of Fog. It is located in front of the Gate of Darkness pillar.`,
         },        
         {
             key: "gree-2",
             type: BadgePartialType.PLAQUE,
-            mapKey: Labyrinth.key,
+            mapKey: TheLabyrinthOfFog.key,
             plaqueType: PlaqueType.MONUMENT,
             location: [584.7, -113.3, 2800.5],
             inscription: `Hope itself became obscured, the land lost in a sea of grey. Few remained to make a stand, for deliverance they could only pray. Then a brilliant plan was proposed, the fog itself could be contained. An infinite vessel it would demand, an eternal prison forever maintained.`,
@@ -49,7 +49,7 @@ export const GreekPhilosopher: IBadgeData = {
         {
             key: "gree-3",
             type: BadgePartialType.PLAQUE,
-            mapKey: Labyrinth.key,
+            mapKey: TheLabyrinthOfFog.key,
             plaqueType: PlaqueType.MONUMENT,
             location: [289.9, -101.3, 2402.0],
             inscription: `The malevolent fog, all-powerful, but forever to be contained. The calamity's will was imposed, with combat would they be entertained. Those who offered their souls, given immortality to forever fight, the strongest Gladiator would earn the fog's favor and might.`,
@@ -58,7 +58,7 @@ export const GreekPhilosopher: IBadgeData = {
         {
             key: "gree-4",
             type: BadgePartialType.PLAQUE,
-            mapKey: Labyrinth.key,
+            mapKey: TheLabyrinthOfFog.key,
             plaqueType: PlaqueType.MONUMENT,
             location: [543.1, 7.0, 2856.0],
             inscription: `The king of fools, for divine wisdom he would not heed. A traitorous, blasphemous act, let none forget his deed. Due to the consequences of thoughtless, brazen sin. The labyrinth gave birth to its immortal guardian.`,
@@ -67,7 +67,7 @@ export const GreekPhilosopher: IBadgeData = {
         {
             key: "gree-5",
             type: BadgePartialType.PLAQUE,
-            mapKey: Labyrinth.key,
+            mapKey: TheLabyrinthOfFog.key,
             plaqueType: PlaqueType.MONUMENT,
             location: [395.0, -245.3, 2748.7],
             inscription: `A horde most unholy, fear given shape, guided by an unseen hand. The time for heroes had come, and many sought to defend the land. Against the endless fog they strove, but it was too dense, too deep. Every monster was cut down, yet no victory could they reap.`,
