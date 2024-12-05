232daea23acc7fd2f9b01250e6b4de474da0c273
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-12-05 12:49:59 -0700

    Update hypothermia.ts
    Added settitle ID

--- src/badge/event/hypothermia.ts
+++ src/badge/event/hypothermia.ts
@@ -3,7 +3,7 @@ import {ALIGNMENT_ANY, BadgeType, IBadgeData} from "coh-content-db";
 export const Hypothermia: IBadgeData = {
     type: BadgeType.EVENT,
     key: "hypothermia",
-    setTitleId: 2518,
+    setTitleId: 2545,
     names: [
         {value: "Hypothermia"},
     ],
