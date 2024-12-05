f6d7a3678488c547c1c4179dc421569784b422fd
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2024-12-05 15:36:59 -0700

    2024 Winter Event (#169)
    * Update hamidons-enemy.ts
    
    typo fix
    
    * Create hypothermia.ts
    
    2024 Winter Event badge
    
    * Update _event-badges.ts
    
    Added 2024 Winter Event badge
    
    * Update hypothermia.ts
    
    Added settitle ID

--- src/badge/accomplishment/hamidons-enemy.ts
+++ src/badge/accomplishment/hamidons-enemy.ts
@@ -10,11 +10,10 @@ export const HamidonsEnemy: IBadgeData = {
     alignment: ALIGNMENT_VILLAIN,
     badgeText: [
         {
-            value: "You went back to Imperial City to find anything useful, but while you were there, you decided " +
-                "to send the Hamidon a message about who it shouldn't mess with."
+            value: `You went back to Imperial City to find anything useful, but while you were there, you decided to send the Hamidon a message about who it shouldn't mess with.`
         }
     ],
-    acquisition: "Defeat a total combination of 40 Devouring Earth or The Infestedin the Inside Information mission",
+    acquisition: "Defeat a total combination of 40 Devouring Earth or The Infested in the Inside Information mission",
     links: [
         {title: "Hamidon's Enemy Badge", href: "https://homecoming.wiki/wiki/Hamidon%27s_Enemy_Badge"}
     ],
