6407ceb2f74da69fdc0aa1fa8a6a030e350d9b20
Nicholas Armstrong <nick@n15g.com>
2025-04-14 19:07:20 +1000

    Output bundle.json during build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,14 @@
 import esbuild from 'rollup-plugin-esbuild'
 import { dts } from 'rollup-plugin-dts'
+import { writeFileSync } from 'node:fs'
+import path from 'node:path'
 
 const name = 'coh-content-db-homecoming'
 
 // noinspection JSUnusedGlobalSymbols
 export default [{
   input: 'src/main/ts/index.ts',
-  plugins: [esbuild()],
+  plugins: [esbuild(), exportBundleJson()],
   output: [{
     file: `dist/${name}.js`,
     format: 'cjs',
@@ -26,3 +28,15 @@ export default [{
   }],
   external: ['coh-content-db'],
 }]
+
+function exportBundleJson() {
+  return {
+    name: 'write-json',
+    async 'buildEnd'() {
+      const bundle = await import(`./dist/${name}.mjs`)
+      const bundlePath = path.resolve('dist', 'bundle.json')
+      // noinspection JSUnresolvedReference
+      writeFileSync(bundlePath, JSON.stringify(bundle.HOMECOMING))
+    },
+  }
+}
