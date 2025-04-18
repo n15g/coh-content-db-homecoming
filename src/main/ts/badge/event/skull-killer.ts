bd27f273e54ac12cecb320429eff0232918e386e
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-04-18 15:21:35 -0600

    Update skull-killer.ts

--- src/main/ts/badge/event/skull-killer.ts
+++ src/main/ts/badge/event/skull-killer.ts
@@ -1,16 +1,15 @@
-import { BadgeData } from "coh-content-db";
+import { BadgeData } from 'coh-content-db'
 
 export const SkullKiller: BadgeData = {
-    type: 'event',
-    key: 'skull-killer',
-    setTitleId: p2554],
-    name: 'Skull Killer',
-    ],
-    morality: 'all',
-    badgeText: `You've relived the past by defeating 25 of the old school Skulls found during the City of Heroes Anniversary Event.`,
-    acquisition: `Defeat 25 old school Skulls spawned from time capsules during the anniversary event.`,
-    links: [
-        {title: "Skull Killer Badge", href: "https://homecoming.wiki/wiki/Skull_Killer_Badge"}
-    ],
-    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/skull-killer.png"}]
-};
+  type: 'event',
+  key: 'skull-killer',
+  setTitleId: [2553],
+  name: 'Skull Killer',
+  morality: 'all',
+  badgeText: `You've relived the past by defeating 25 of the old school Skulls found during the City of Heroes Anniversary Event.`,
+  acquisition: `Defeat 25 old school Skulls spawned from time capsules during the anniversary event.`,
+  links: [
+    { title: 'Skull Killer', href: 'https://homecoming.wiki/wiki/Skull_Killer_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/skull-killer.png',
+}
