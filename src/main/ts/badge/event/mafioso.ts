88ecd5172882a8812e6df1da462c1faa8b277250
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-04-18 15:20:30 -0600

    Update mafioso.ts

--- src/main/ts/badge/event/mafioso.ts
+++ src/main/ts/badge/event/mafioso.ts
@@ -1,15 +1,15 @@
-import { BadgeData } from 'coh-content-db'
+import { BadgeData, badgeLink } from 'coh-content-db'
 
-export const Mafioso: BadgeData = {
-    type: 'event',
-    key: 'mafioso',
-    setTitleId: [2553],
-    name: 'Mafioso',
-    morality: 'all',
-    badgeText: `You've relived the past by defeating 25 of the old school Family found during the City of Heroes Anniversary Event.`,
-    acquisition: `Defeat 25 old school Family spawned from time capsules during the anniversary event.`,
-    links: [
-        {title: "Mafioso Badge", href: "https://homecoming.wiki/wiki/Mafioso_Badge"}
-    ],
-    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/mafioso.png"}]
-};
+export const ChaosSpiel: BadgeData = {
+  type: 'event',
+  key: 'mafioso',
+  setTitleId: [2553],
+  name: 'Mafioso',
+  morality: 'all',
+  badgeText: `You've relived the past by defeating 25 of the old school Family found during the City of Heroes Anniversary Event.`,
+  acquisition: `Defeat 25 old school Family spawned from time capsules during the anniversary event.`,
+  links: [
+    { title: 'Mafioso', href: 'https://homecoming.wiki/wiki/Mafioso_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/mafioso.png',
+}
