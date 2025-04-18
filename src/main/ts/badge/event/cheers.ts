e02667427600794c695dfc1562047a84bcb91b71
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-04-18 15:08:00 -0600

    Update cheers.ts

--- src/main/ts/badge/event/cheers.ts
+++ src/main/ts/badge/event/cheers.ts
@@ -1,15 +1,15 @@
 import { BadgeData } from 'coh-content-db'
 
 export const Cheers: BadgeData = {
-    type: 'event',
-    key: "cheers",
-    setTitleId: [2552],
-    name: 'Cheers!',
-    morality: 'all',
-    badgeText: `You have helped celebrate the 21st anniversary of City of Heroes.`,
-    acquisition: `Awarded upon login during May 2025, and thereafter available for purchase from Luna in ${zoneLink(Ouroboros)} during the anniversary event in May.`,
-    links: [
-        {title: "Cheers! Badge", href: "https://homecoming.wiki/wiki/Cheers_Badge"}
-    ],
-    icons: [{value: "https://n15g.github.io/coh-content-db-homecoming/images/badges/event/cheers.png"}]
-};
+  type: 'event',
+  key: 'cheers',
+  setTitleId: [2552],
+  name: 'Cheers!',
+  morality: 'all',
+  badgeText: `You have helped celebrate the 21st anniversary of City of Heroes.`,
+  acquisition: `Awarded upon login during May 2025, and thereafter available for purchase from Luna in ${zoneLink(Ouroboros)} during the anniversary event in May.`,
+  links: [
+    { title: 'Cheers!', href: 'https://homecoming.wiki/wiki/Cheers_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/cheers.png',
+}
