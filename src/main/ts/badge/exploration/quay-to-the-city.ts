e4c940bd8439aa9530c673733f4ad4b9c4699130
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-06-01 14:28:24 -0600

    Update quay-to-the-city.ts

--- src/main/ts/badge/exploration/quay-to-the-city.ts
+++ src/main/ts/badge/exploration/quay-to-the-city.ts
@@ -11,7 +11,7 @@ export const ProtestTooMuch: BadgeData = {
   badgeText: `Kallisti Marina is all that is left of a larger harbor that originally gave Kallisti Wharf half of its name. The first half, Kallisti, is a greek word that means 'The prettiest one'. Back when it gained the moniker, there were many Greek immigrants to the area, and it was noted for its placid, beautiful waters, bright blue, full of fish and free of the pollution that would come later. Before Independence Port, this Marina was the first port of call for imports and immigrants alike`,
   notes: ` ??? `,
   links: [
-    { title: 'Quay to the City Badge', href: 'https://homecoming.wiki/wiki/Quay_To_The City_Badge' },
+    { title: 'Quay to the City Badge', href: 'https://homecoming.wiki/wiki/Quay_To_The_City_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/exploration/hero.png',
   requirements: [
