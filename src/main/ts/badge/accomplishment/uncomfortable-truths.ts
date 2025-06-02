c11cf732ba4e48664689f5dc32cc59c26f0b0681
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-06-02 14:10:28 -0600

    Fix typo

--- src/main/ts/badge/accomplishment/uncomfortable-truths.ts
+++ src/main/ts/badge/accomplishment/uncomfortable-truths.ts
@@ -1,19 +1,19 @@
 import { BadgeData } from 'coh-content-db'
 import { CuttingBackOldGrowth } from '../../mission/cutting-back-old-growth'
 
-export const UnfomfortableTruths: BadgeData = {
+export const UncomfortableTruths: BadgeData = {
   type: 'accomplishment',
-  key: 'unfomfortable-truths',
+  key: 'uncomfortable-truths',
   setTitleId: [2565],
-  name: 'Unfomfortable Truths',
+  name: 'Uncomfortable Truths',
   releaseDate: '2025-06-??',
   morality: 'villainous',
   badgeText: `Sometimes the only thing it takes to completely rock a person's world is to learn a few uncomfortable truths.`,
   acquisition: `Complete bonus objectives Erin West's story arc Cutting Back Old Growth.`,
   links: [
-    { title: 'Unfomfortable Truths Badge', href: 'https://homecoming.wiki/wiki/Unfomfortable_Truths_Badge' },
+    { title: 'Uncomfortable Truths Badge', href: 'https://homecoming.wiki/wiki/Uncomfortable_Truths_Badge' },
   ],
-  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/unfomfortable-truths.png',
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/uncomfortable-truths.png',
   requirements: [
     { key: CuttingBackOldGrowth.key, type: 'mission', missionKey: CuttingBackOldGrowth.key },
   ],
