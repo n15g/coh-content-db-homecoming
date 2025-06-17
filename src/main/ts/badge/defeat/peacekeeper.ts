aceb96c97e360e7e75c09f70762f5750438c5797
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-06-17 11:43:36 -0600

    Update peacekeeper.ts

--- src/main/ts/badge/defeat/peacekeeper.ts
+++ src/main/ts/badge/defeat/peacekeeper.ts
@@ -7,7 +7,7 @@ export const Peacekeeper: BadgeData = {
   name: [
     { alignment: 'hero', value: 'Peacekeeper' },
     { alignment: 'villain', value: `Peacemaker` },
-  ],  
+  ],
   releaseDate: '2025-06-17',
   morality: 'all',
   badgeText: `The most expensive weapon in P.E.A.C.E.'s arsenal was a gargantuan piloted mech... That you sent to the scrap heap. It's not quite apparent what One Statesman Plaza and its tower hides, but Blackwing Industries have been paid an unthinkable sum to ensure that Emil Christie's investment was secure.`,
