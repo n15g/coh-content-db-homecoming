970b94ed21eaee450de1e5f4860e80b789bb3202
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-06-17 11:38:37 -0600

    Update peacekeeper.ts

--- src/main/ts/badge/defeat/peacekeeper.ts
+++ src/main/ts/badge/defeat/peacekeeper.ts
@@ -10,7 +10,7 @@ export const Peacekeeper: BadgeData = {
   ],  
   releaseDate: '2025-06-17',
   morality: 'all',
-  badgeText: 'The most expensive weapon in P.E.A.C.E.'s arsenal was a gargantuan piloted mech... That you sent to the scrap heap. It's not quite apparent what One Statesman Plaza and its tower hides, but Blackwing Industries have been paid an unthinkable sum to ensure that Emil Christie's investment was secure.',
+  badgeText: `The most expensive weapon in P.E.A.C.E.'s arsenal was a gargantuan piloted mech... That you sent to the scrap heap. It's not quite apparent what One Statesman Plaza and its tower hides, but Blackwing Industries have been paid an unthinkable sum to ensure that Emil Christie's investment was secure.`,
   acquisition: 'Defeat the P.E.A.C.E. Keeper in Kallisti Wharf to earn this badge.',
   links: [
     { title: 'Peacekeeper Badge', href: 'https://homecoming.wiki/wiki/Peacekeeper_Badge' },
