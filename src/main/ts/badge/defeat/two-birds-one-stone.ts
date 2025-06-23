6a3f075e4348e0baa16226f78b7a5c6002dd0cc1
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-06-22 23:48:39 -0600

    Update two-birds-one-stone.ts
    Added missing comma to name

--- src/main/ts/badge/defeat/two-birds-one-stone.ts
+++ src/main/ts/badge/defeat/two-birds-one-stone.ts
@@ -4,14 +4,14 @@ export const TwoBirdsOneStone: BadgeData = {
   type: 'defeat',
   key: 'two-birds-one-stone',
   setTitleId: [2508],
-  name: 'Two Birds One Stone',
+  name: 'Two Birds, One Stone',
   releaseDate: '2025-06-17',
   morality: 'all',
   badgeText: `When the Blackwing Industries private military company was contracted to protect Kallisti Wharf, you weren't sure what to expect.
 Now, having faced their advanced armor, weapons, and training yourself, there's no doubt that the security of Kallisti Wharf is in capable hands.`,
   acquisition: 'Defeat 250 Blackwing Industries enemies.',
   links: [
-    { title: 'Two Birds One Stone Badge', href: 'https://homecoming.wiki/wiki/Two_Birds_One_Stone_Badge' },
+    { title: 'Two Birds, One Stone Badge', href: 'https://homecoming.wiki/wiki/Two_Birds_One_Stone_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/two-birds-one-stone.png',
 }
