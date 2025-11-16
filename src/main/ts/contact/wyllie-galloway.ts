5bb505cb24596e9f31ef2e473043bbe49eca735e
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:09:04 -0800

    Update wyllie-galloway.ts

--- src/main/ts/contact/wyllie-galloway.ts
+++ src/main/ts/contact/wyllie-galloway.ts
@@ -6,7 +6,8 @@ export const WyllieGalloway: ContactData = {
   name: 'Wyllie Galloway',
   title: 'Mystic Librarian',
   morality: 'heroic',
-  location: { zoneKey: KallistiWharf.key, coords: [3564 84 3295] },
+  location: { zoneKey: KallistiWharf.key, coords: [3564, 84, 3295] },
   levelRange: [40, 50],
   links: [{ title: 'Wyllie Galloway', href: 'https://homecoming.wiki/wiki/Wyllie_Galloway' }],
+
 }
