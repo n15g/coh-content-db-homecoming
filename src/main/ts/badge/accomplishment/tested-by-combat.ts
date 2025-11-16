d5be2cf74d00e7d459ac5964934c89ca0d0ebfca
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 16:59:15 -0800

    Update tested-by-combat.ts

--- src/main/ts/badge/accomplishment/tested-by-combat.ts
+++ src/main/ts/badge/accomplishment/tested-by-combat.ts
@@ -7,10 +7,11 @@ export const TestedByCombat: BadgeData = {
   name: 'Tested By Combat',
   releaseDate: '2025-11-30',
   morality: 'all',
-  badgeText: 'You've proven yourself adapative and a worthy challenger by overcoming the many different Tests of Combat offered by the Four Generals of Achlys in the Labyrinth of Fog.',
-  acquisition: 'Complete all six standard Tests of Combat offered by The Watcher, Teleute in the Core of the Labyrinth of Fog.',
+  badgeText: `You've proven yourself adapative and a worthy challenger by overcoming the many different Tests of Combat offered by the Four Generals of Achlys in the Labyrinth of Fog.`,
+  acquisition: `Complete all six standard Tests of Combat offered by The Watcher, Teleute in the Core of the Labyrinth of Fog.`,
   links: [
     { title: 'Tested By Combat Badge', href: 'https://homecoming.wiki/wiki/Tested_By_Combat_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/tested-by-combat.png',
+
 }
