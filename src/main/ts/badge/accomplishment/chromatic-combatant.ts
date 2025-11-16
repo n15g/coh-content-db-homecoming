28b842e81624496886c0b49f0a661f78033ceb20
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 16:57:36 -0800

    Update chromatic-combatant.ts

--- src/main/ts/badge/accomplishment/chromatic-combatant.ts
+++ src/main/ts/badge/accomplishment/chromatic-combatant.ts
@@ -7,10 +7,11 @@ export const ChromaticCombatant: BadgeData = {
   name: 'Chromatic Combatant',
   releaseDate: '2025-11-30',
   morality: 'all',
-  badgeText: 'You've shown yourself amongst greatest of your otherworldeer peers by mastering the Tests of Combat. The phantoms knows your true worth after defeating all Four Generals of Achlys at the same time.',
-  acquisition: 'Complete the Test of Apathy offered by The Watcher, Teleute in the Core of the Labyrinth of Fog.',
+  badgeText: `You've shown yourself amongst greatest of your otherworldeer peers by mastering the Tests of Combat. The phantoms knows your true worth after defeating all Four Generals of Achlys at the same time.`,
+  acquisition: `Complete the Test of Apathy offered by The Watcher, Teleute in the Core of the Labyrinth of Fog.`,
   links: [
     { title: 'Chromatic Combatant Badge', href: 'https://homecoming.wiki/wiki/Chromatic_Combatant_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/chromatic-combatant.png',
+
 }
