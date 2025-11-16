a9d34c9b090f1de6da12bab87e569164408fd57b
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 16:48:52 -0800

    Add files via upload

---
+++ src/main/ts/badge/achievement/master-of-zoes-task-force.ts
@@ -0,0 +1,16 @@
+import { BadgeData } from 'coh-content-db'
+
+export const MasterOfZoesTaskForce: BadgeData = {
+  type: 'achievement',
+  key: 'master-of-zoes-task-force',
+  setTitleId: [2581],
+  name: 'Master of Zoe's Task Force',
+  releaseDate: '2025-11-30',
+  morality: 'any',
+  badgeText: 'You have proven yourself Master of Marigold 'Zoe' Langston's Task Force.',
+  acquisition: 'Achieve the following special achievements during Marigold 'Zoe' Langston's Task Force: Snow Warning, Jailbreaker, Subterranean Striker, and Knockback King/Queen.',
+  links: [
+    { title: 'Master of Zoe's Task Force Badge', href: 'https://homecoming.wiki/wiki/Master_of_Zoe's_Task_Force_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/achievement/master-of-zoes-task-force.png',
+}
