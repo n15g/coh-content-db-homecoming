64ac9c81823183e237039ac5d85c07122a8d0c34
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 16:44:11 -0800

    Add files via upload

---
+++ src/main/ts/contact/zoe-langston.ts
@@ -0,0 +1,12 @@
+import { ContactData } from 'coh-content-db'
+import { KallistiWharf } from '../zone/kallisti-wharf'
+
+export const ZoeLangston: ContactData = {
+  key: 'zoe-langston',
+  name: 'Marigold 'Zoe' Langston',
+  title: 'Heiress to the Langston Dynasty',
+  morality: 'any',
+  location: { zoneKey: KallistiWharf.key, coords: [475.0 -280.0 3899.0] },
+  levelRange: [40, 50],
+  links: [{ title: 'Marigold 'Zoe' Langston', href: 'https://homecoming.wiki/wiki/Zoe_Langston' }],
+}
