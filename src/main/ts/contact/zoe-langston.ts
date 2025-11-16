d7e3318956dd50f8b42f4c82d3b78d251f4339b5
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:26:28 -0800

    Update zoe-langston.ts

--- src/main/ts/contact/zoe-langston.ts
+++ src/main/ts/contact/zoe-langston.ts
@@ -5,7 +5,7 @@ export const ZoeLangston: ContactData = {
   key: 'zoe-langston',
   name: `Marigold 'Zoe' Langston`,
   title: 'Heiress to the Langston Dynasty',
-  morality: 'any',
+  morality: 'all',
   location: { zoneKey: KallistiWharf.key, coords: [475, -280, 3899] },
   levelRange: [40, 50],
   links: [{ title: `Marigold 'Zoe' Langston`, href: `https://homecoming.wiki/wiki/Zoe_Langston` }],
