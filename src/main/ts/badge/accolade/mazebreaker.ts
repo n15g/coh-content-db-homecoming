34e080a0d5b7e8633ec4a76b3f4a23cd51cc0c4e
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 16:47:16 -0800

    Add files via upload

---
+++ src/main/ts/badge/accolade/mazebreaker.ts
@@ -0,0 +1,22 @@
+import { BadgeData } from 'coh-content-db'
+import { TestedByCombat } from '../exploration/tested-by-combat'
+import { ChromaticCombatant } from '../achievement/chromatic-combatant
+
+export const Mazebreaker: BadgeData = {
+  type: 'accolade',
+  key: 'mazebreaker',
+  setTitleId: [2582],
+  name: 'Mazebreaker',
+  releaseDate: '2025-11-30',
+  morality: 'all',
+  badgeText: `You've become much more adept at fighting in the Labyrinth of Fog after undergoing the full spectrum of Tests of Combat offered by the Four Generals of Achlys. No matter what comes next for the dream-like maze, you'll be ready.`,
+  effect: `Awards the Mark and Recall power.`,
+  links: [
+    { title: 'Mazebreaker Badge', href: 'https://homecoming.wiki/wiki/Mazebreaker_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accolade/mazebreaker-realities.png',
+  requirements: [
+    { key: TestedByCombat.key, type: 'badge', badgeKey: TestedByCombat.key },
+    { key: ChromaticCombatant.key, type: 'badge', badgeKey: ChromaticCombatant.key },
+  ],
+}
