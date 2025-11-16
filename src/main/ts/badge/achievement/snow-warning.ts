ef644457a2063e81e4405204018ebdf96659f412
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:05:21 -0800

    Update snow-warning.ts

--- src/main/ts/badge/achievement/snow-warning.ts
+++ src/main/ts/badge/achievement/snow-warning.ts
@@ -2,15 +2,16 @@ import { BadgeData } from 'coh-content-db'
 
 export const SnowWarning: BadgeData = {
   type: 'achievement',
-  key: 'snow-warning,
+  key: 'snow-warning',
   setTitleId: [2577],
   name: 'Snow Warning',
   releaseDate: '2025-11-30',
   morality: 'any',
-  badgeText: 'You've proven to be light on your feet, dodging all of the deadly cones of cold sent your way by Ultimatum's Flashfreeze, the agent who holds commanding authority over Blackwing Industries' P.E.A.C.E. division.',
-  acquisition: 'Defeat Flashfreeze in Marigold 'Zoe' Langston's Task Force on Advanced Mode without anybody on the team being hit by her Cryonic Judgement special attack.Complete Marigold 'Zoe' Langston's Task Force.',
+  badgeText: `You've proven to be light on your feet, dodging all of the deadly cones of cold sent your way by Ultimatum's Flashfreeze, the agent who holds commanding authority over Blackwing Industries' P.E.A.C.E. division.`,
+  acquisition: `Defeat Flashfreeze in Marigold 'Zoe' Langston's Task Force on Advanced Mode without anybody on the team being hit by her Cryonic Judgement special attack.Complete Marigold 'Zoe' Langston's Task Force.`,
   links: [
     { title: 'SnowWarning Badge', href: 'https://homecoming.wiki/wiki/Snow_Warning_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/achievement/tf-zoe.png',
+
 }
