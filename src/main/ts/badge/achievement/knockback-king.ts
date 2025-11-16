c7a5ceb89a75cac34ec861a4cb6b6aeeca6901bf
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:00:23 -0800

    Update knockback-king.ts

--- src/main/ts/badge/achievement/knockback-king.ts
+++ src/main/ts/badge/achievement/knockback-king.ts
@@ -10,10 +10,11 @@ export const KnockbackKing: BadgeData = {
   ],
   releaseDate: '2025-11-30',
   morality: 'any',
-  badgeText: 'While many working supers consider knockback an annoyance, you see it as an opportunity to put your opponents right where you want them. The Nemesis Army have learned a valuable lesson on why you should keep your arms and legs inside the moving train at all times.',
-  acquisition: 'Defeat 50 enemies via [CRUNCH] on the final mission of Marigold 'Zoe' Langston's Task Force.',
+  badgeText: `While many working supers consider knockback an annoyance, you see it as an opportunity to put your opponents right where you want them. The Nemesis Army have learned a valuable lesson on why you should keep your arms and legs inside the moving train at all times.`,
+  acquisition: `Defeat 50 enemies via [CRUNCH] on the final mission of Marigold 'Zoe' Langston's Task Force.`,
   links: [
     { title: 'Knockback King Badge', href: 'https://homecoming.wiki/wiki/Knockback_King_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/achievement/tf-zoe.png',
+
 }
