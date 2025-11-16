d923a96bd8db122b54c38ea7f9c86590c6f9ea68
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:27:31 -0800

    Update snow-warning.ts

--- src/main/ts/badge/achievement/snow-warning.ts
+++ src/main/ts/badge/achievement/snow-warning.ts
@@ -6,7 +6,7 @@ export const SnowWarning: BadgeData = {
   setTitleId: [2577],
   name: 'Snow Warning',
   releaseDate: '2025-11-30',
-  morality: 'any',
+  morality: 'all',
   badgeText: `You've proven to be light on your feet, dodging all of the deadly cones of cold sent your way by Ultimatum's Flashfreeze, the agent who holds commanding authority over Blackwing Industries' P.E.A.C.E. division.`,
   acquisition: `Defeat Flashfreeze in Marigold 'Zoe' Langston's Task Force on Advanced Mode without anybody on the team being hit by her Cryonic Judgement special attack.Complete Marigold 'Zoe' Langston's Task Force.`,
   links: [
