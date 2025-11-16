812f4b8a88f2033b5309aeae34672b9241af259a
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:06:26 -0800

    Update mutated-marauder.ts

--- src/main/ts/badge/defeat/mutated-marauder.ts
+++ src/main/ts/badge/defeat/mutated-marauder.ts
@@ -7,10 +7,11 @@ export const MutatedMarauder: BadgeData = {
   name: 'Mutated Maurader',
   releaseDate: '2025-11-30',
   morality: 'all',
-  badgeText: 'There's so many conflicting theories as to the true origin of the Hydra that you aren't sure what to believe. What you are sure of, however, is that these monstrous mutated organisms are incredibly dangerous.',
-  acquisition: 'Defeat 100 Hydra enemies.',
+  badgeText: `There's so many conflicting theories as to the true origin of the Hydra that you aren't sure what to believe. What you are sure of, however, is that these monstrous mutated organisms are incredibly dangerous.`,
+  acquisition: `Defeat 100 Hydra enemies.`,
   links: [
     { title: 'Mutated Marauder Badge', href: 'https://homecoming.wiki/wiki/Mutated_Maurader_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/mutated-marauder.png',
+
 }
