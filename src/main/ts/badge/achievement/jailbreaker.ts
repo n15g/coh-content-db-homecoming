107cf1c91ba3c78a97146a86ff404cb3755d5cb0
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-15 17:27:55 -0800

    Update jailbreaker.ts

--- src/main/ts/badge/achievement/jailbreaker.ts
+++ src/main/ts/badge/achievement/jailbreaker.ts
@@ -6,7 +6,7 @@ export const Jailbreaker: BadgeData = {
   setTitleId: [2578],
   name: 'Jailbreaker',
   releaseDate: '2025-11-30',
-  morality: 'any',
+  morality: 'all',
   badgeText: `Even though getting the Golden Roller out of prison didn't go entirely according to plan, you still ensured none of the other dangerous threats contained within the Arachnos Central Holding Block could escape back into the world.`,
   acquisition: `Defeat all five unique CHB prisoner arch-villains encountered during Marigold 'Zoe' Langston's Task Force.`,
   links: [
