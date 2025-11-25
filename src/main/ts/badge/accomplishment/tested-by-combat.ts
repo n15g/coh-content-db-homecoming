7bb689dc86c520f0ec8c05a551df14f298b44016
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-25 07:51:54 -0800

    Update tested-by-combat.ts

--- src/main/ts/badge/accomplishment/tested-by-combat.ts
+++ src/main/ts/badge/accomplishment/tested-by-combat.ts
@@ -5,7 +5,7 @@ export const TestedByCombat: BadgeData = {
   key: 'tested-by-combat',
   setTitleId: [2575],
   name: 'Tested By Combat',
-  releaseDate: '2025-11-30',
+  releaseDate: '2025-11-25',
   morality: 'all',
   badgeText: `You've proven yourself adapative and a worthy challenger by overcoming the many different Tests of Combat offered by the Four Generals of Achlys in the Labyrinth of Fog.`,
   acquisition: `Complete all six standard Tests of Combat offered by The Watcher, Teleute in the Core of the Labyrinth of Fog.`,
