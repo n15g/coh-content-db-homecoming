0ec0e496d390370cf3e1d454506f8503661a3f45
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-25 07:53:25 -0800

    Update knockback-king.ts

--- src/main/ts/badge/achievement/knockback-king.ts
+++ src/main/ts/badge/achievement/knockback-king.ts
@@ -8,7 +8,7 @@ export const KnockbackKing: BadgeData = {
     { sex: 'M', value: 'Knockback King' },
     { sex: 'F', value: 'Knockback Queen' },
   ],
-  releaseDate: '2025-11-30',
+  releaseDate: '2025-11-25',
   morality: 'all',
   badgeText: `While many working supers consider knockback an annoyance, you see it as an opportunity to put your opponents right where you want them. The Nemesis Army have learned a valuable lesson on why you should keep your arms and legs inside the moving train at all times.`,
   acquisition: `Defeat 50 enemies via [CRUNCH] on the final mission of Marigold 'Zoe' Langston's Task Force.`,
