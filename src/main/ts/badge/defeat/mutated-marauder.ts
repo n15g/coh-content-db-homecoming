8cc5bcc3c7ae9020dbeeb415b935065559f2a7ec
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-25 07:54:03 -0800

    Update mutated-marauder.ts

--- src/main/ts/badge/defeat/mutated-marauder.ts
+++ src/main/ts/badge/defeat/mutated-marauder.ts
@@ -5,7 +5,7 @@ export const MutatedMarauder: BadgeData = {
   key: 'mutated-marauder',
   setTitleId: [2583],
   name: 'Mutated Maurader',
-  releaseDate: '2025-11-30',
+  releaseDate: '2025-11-25',
   morality: 'all',
   badgeText: `There's so many conflicting theories as to the true origin of the Hydra that you aren't sure what to believe. What you are sure of, however, is that these monstrous mutated organisms are incredibly dangerous.`,
   acquisition: `Defeat 100 Hydra enemies.`,
