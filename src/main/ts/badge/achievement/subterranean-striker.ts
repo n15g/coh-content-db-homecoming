88e7e809aa585b730f6a810c80d8b85753064b89
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2025-11-25 07:53:09 -0800

    Update subterranean-striker.ts

--- src/main/ts/badge/achievement/subterranean-striker.ts
+++ src/main/ts/badge/achievement/subterranean-striker.ts
@@ -5,7 +5,7 @@ export const SubterraneanStriker: BadgeData = {
   key: 'subterranean-striker',
   setTitleId: [2579],
   name: 'Subterranean Striker',
-  releaseDate: '2025-11-30',
+  releaseDate: '2025-11-25',
   morality: 'all',
   badgeText: `An unfathomable amount of dangers slither in the dark below Paragon City, but these threats didn't deter you in the slightest. You carved a path through the tunnels below Kallisti Wharf, eliminating every potential threat to the surface.`,
   acquisition: `Defeat all three arch-villain enemies while exploring underground in Marigold 'Zoe' Langston's Task Force on Advanced Mode.`,
