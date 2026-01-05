bc01d098e5245620be0bf4572d1e4730f96a641a
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-01-05 07:04:57 -0800

    Update winter-weathered.ts

--- src/main/ts/badge/event/winter-weathered.ts
+++ src/main/ts/badge/event/winter-weathered.ts
@@ -8,9 +8,10 @@ export const WinterWeathered: BadgeData = {
   releaseDate: '2025-12-01',
   morality: 'all',
   badgeText: `You have braved the frosty villains and icy perils of the holiday season! Your dedication remains unmelted despite the winter chill.`,
-  acquisition: 'Log in during the 2025 Winter Event, or purchase from the Candy Keeper for 25 Candy Canes and 30,000 inf.',
+  acquisition: `Log in during the 2025 Winter Event, or purchase from the Candy Keeper for 25 Candy Canes and 30,000 inf.`,
   links: [
     { title: 'Winter Weathered Badge', href: 'https://homecoming.wiki/wiki/Winter_Weathered_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/winter-weathered.png',
+
 }
