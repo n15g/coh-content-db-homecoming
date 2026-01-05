7d154a02841585eff416785577585fad627778f8
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-01-05 07:04:21 -0800

    Update icebreaker.ts

--- src/main/ts/badge/event/icebreaker.ts
+++ src/main/ts/badge/event/icebreaker.ts
@@ -7,10 +7,11 @@ export const Icebreaker: BadgeData = {
   name: 'Icebreaker',
   releaseDate: '2025-12-01',
   morality: 'all',
-  badgeText: 'Lord Winter's plan to consolidate his power was shattered, denying him the full might of the season.',
-  acquisition: 'Defeat Lord Winter without letting his Winter Essence ever reach 100%.',
+  badgeText: `Lord Winter's plan to consolidate his power was shattered, denying him the full might of the season.`,
+  acquisition: `Defeat Lord Winter without letting his Winter Essence ever reach 100%.`,
   links: [
     { title: 'Icebreaker Badge', href: 'https://homecoming.wiki/wiki/Icebreaker_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/icebreaker.png',
+
 }
