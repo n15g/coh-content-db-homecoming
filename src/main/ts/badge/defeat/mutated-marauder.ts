4898c0556c0ac49f49a4e6575051351761ad88c2
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-01-13 19:03:23 -0800

    Mutated Marauder spelling correction (#184)

--- src/main/ts/badge/defeat/mutated-marauder.ts
+++ src/main/ts/badge/defeat/mutated-marauder.ts
@@ -4,14 +4,13 @@ export const MutatedMarauder: BadgeData = {
   type: 'defeat',
   key: 'mutated-marauder',
   setTitleId: [2583],
-  name: 'Mutated Maurader',
+  name: 'Mutated Marauder',
   releaseDate: '2025-11-25',
   morality: 'all',
   badgeText: `There's so many conflicting theories as to the true origin of the Hydra that you aren't sure what to believe. What you are sure of, however, is that these monstrous mutated organisms are incredibly dangerous.`,
   acquisition: `Defeat 100 Hydra enemies.`,
   links: [
-    { title: 'Mutated Marauder Badge', href: 'https://homecoming.wiki/wiki/Mutated_Maurader_Badge' },
+    { title: 'Mutated Marauder Badge', href: 'https://homecoming.wiki/wiki/Mutated_Marauder_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/defeat/mutated-marauder.png',
-
 }
