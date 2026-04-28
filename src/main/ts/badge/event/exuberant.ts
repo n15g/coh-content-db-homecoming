bb4c368ba05c3a72b86b6ca5cafca0adc0bc42a2
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-04-27 17:31:03 -0700

    2026 Anniversary event badge files

---
+++ src/main/ts/badge/event/exuberant.ts
@@ -0,0 +1,16 @@
+import { BadgeData } from 'coh-content-db'
+
+export const Exuberant: BadgeData = {
+  type: 'event',
+  key: 'Exuberant',
+  setTitleId: [2585],
+  name: 'Exuberant',
+  releaseDate: '2026-04-19',
+  morality: 'all',
+  badgeText: `You have helped celebrate the 22st anniversary of City of Heroes.`,
+  acquisition: `Awarded upon login during May 2026, and thereafter available for purchase from Luna in Ouroboros during the anniversary event in May.`,
+  links: [
+    { title: 'Exuberant', href: 'https://homecoming.wiki/wiki/Exuberant_Badge' },
+  ],
+  icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/event/exuberant.png',
+}
